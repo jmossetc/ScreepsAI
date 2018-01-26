faf2b0e709f2a9e11ff61e98d6c6ce23028407df
Unknown <jeremie.mossetchafik@gmail.com>
2018-01-26 23:43:40 +0100

    will only construct containers actually repair take   energy from source when there is no containers  with energy

--- role.builder.js
+++ role.builder.js
@@ -1,46 +1,64 @@
 var roleBuilder = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-
-        if(creep.memory.building && creep.carry.energy == 0) {
+    run: function (creep) {
+        //Switching between building and harvest mode
+        if (creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
             creep.say('🔄 harvest');
         }
-        else if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+        else if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
             creep.memory.building = true;
             creep.say('🚧 build');
         }
 
-        if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+        if (creep.memory.building) {
+            //Finding containers
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER);
+                }
+            });
+            if (targets.length && creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+            else {
+                //Repairing structures as a fallback
+                targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (
+                            structure.hits < structure.hitsMax
+                        );
+                    }
+                });
+                if (targets.length && creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
-            else {
-                targets = creep.room.find(FIND_STRUCTURES, {
-                    filter:(structure) =>{
-                    return (
-                        structure.hits  <  structure.hitsMax
-                        );
-                }
-                });
-            }
         }
         else {
+            //Find containers with energy
             var sources = creep.room.find(FIND_STRUCTURES, {
-                filter:(structure) =>{
+                filter: (structure) => {
                     return (
                         structure.structureType == STRUCTURE_CONTAINER &&
                         structure.store.energy > 0
-                        );
+                    );
                 }
             });
-            var withdrawCode = creep.withdraw(sources[0]);
-            if(creep.withdraw(sources[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+
+            if (sources.length) {
+                var withdrawCode = creep.withdraw(sources[0], RESOURCE_ENERGY);
+                if (withdrawCode == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+            else {
+                //Retrieve energy from source
+                sources = creep.room.find(FIND_SOURCES);
+                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
             }
         }
     }
