5dad1efd0c2ec6072dd2cf347f56e85c414036d1
Unknown <jeremie.mossetchafik@gmail.com>
2018-01-27 07:22:39 +0100

    repairer role

---
+++ role.repairer.js
@@ -0,0 +1,64 @@
+var roleRepairer = {
+
+        /** @param {Creep} creep **/
+        run: function (creep) {
+            //Switching between repair and harvest mode
+            if (creep.memory.working && creep.carry.energy == 0) {
+                creep.memory.working = false;
+            }
+            else if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+                creep.memory.working = true;
+            }
+
+            if (creep.memory.working) {
+                //Sorting structures by hit point ratio
+                targets = _.sortBy(creep.room.find(FIND_MY_STRUCTURES), structure => (structure.hits / structure.hitsMax));
+                if (targets.length) {
+                    if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+                else {
+                    /*
+                    //Getting containers by hp ratio
+                    targets = _.sortBy(creep.room.find(FIND_STRUCTURES, {
+                        filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_CONTAINER);
+                        }
+                    }), structure => (structure.hits / structure.hitsMax));
+                    if (targets.length) {
+                        if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                        }
+                    }*/
+                }
+            }
+            else {
+                //Find containers with energy
+                let sources = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (
+                            structure.structureType == STRUCTURE_CONTAINER &&
+                            structure.store.energy > 0
+                        );
+                    }
+                });
+
+                if (sources.length) {
+                    if (creep.withdraw(sources[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
+                }
+                else {
+                    //Retrieve energy from source
+                    sources = creep.room.find(FIND_SOURCES);
+                    if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
+                }
+            }
+        }
+    }
+;
+
+module.exports = roleRepairer;
