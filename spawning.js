90b9847b406b3db4ab65e963451aa55b80b3d127
Unknown <jeremie.mossetchafik@gmail.com>
2018-01-28 01:53:54 +0100

    Do not call createCreepWithCustomRole  when not needed

--- spawning.js
+++ spawning.js
@@ -10,8 +10,8 @@ require('prototype.StructureSpawn')();
 
 const MAX_HARVESTERS = 4;
 const MAX_REPAIRERS = 1;
-const MAX_BUILDERS = 1;
-const MAX_UPGRADERS = 4;
+const MAX_BUILDERS = 2;
+const MAX_UPGRADERS = 2;
 const MIN_ENERGY = 200;
 
 var spawning = {
@@ -27,21 +27,28 @@ var spawning = {
         let energy = Game.spawns[spawnName].room.energyCapacityAvailable;
         let energyAvailable = Game.spawns[spawnName].room.energyAvailable;
         if (energyAvailable > MIN_ENERGY) {
-            let harvestersNumber = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;
-            if (harvestersNumber < MAX_HARVESTERS) {
-                if (harvestersNumber == 0) {
-                    Game.spawns[spawnName].createCreepWithCustomRole(energyAvailable, 'harvester');
+            let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+            let repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+            let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+            let builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+            if (harvesters.length < MAX_HARVESTERS) {
+                if (harvesters.length == 0) {
+                    Game.spawns[spawnName].createCreepWithCustomRole(energyAvailable, 'harvester', harvesters);
                 }
-                Game.spawns[spawnName].createCreepWithCustomRole(energy, 'harvester');
+                else if(energyAvailable == energy) {
+                    Game.spawns[spawnName].createCreepWithCustomRole(energy, 'harvester', harvesters);
+                }
             }
-            else if (_.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length < MAX_REPAIRERS) {
-                Game.spawns[spawnName].createCreepWithCustomRole(energy, 'repairer');
-            }
-            else if (_.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length < MAX_UPGRADERS) {
-                Game.spawns[spawnName].createCreepWithCustomRole(energy, 'upgrader');
-            }
-            else if (_.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length < MAX_BUILDERS) {
-                Game.spawns[spawnName].createCreepWithCustomRole(energy, 'builder');
+            else if(energyAvailable == energy) {
+                if (repairers.length < MAX_REPAIRERS) {
+                    Game.spawns[spawnName].createCreepWithCustomRole(energy, 'repairer', undefined);
+                }
+                else if (builders.length < MAX_BUILDERS) {
+                    Game.spawns[spawnName].createCreepWithCustomRole(energy, 'builder', undefined);
+                }
+                else if (upgraders.length < MAX_UPGRADERS) {
+                    Game.spawns[spawnName].createCreepWithCustomRole(energy, 'upgrader', undefined);
+                }
             }
         }
     }
